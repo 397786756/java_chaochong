36e462ed457a7906cdc240066f124284a20b5381
zhengsongjie <397786756.com>
2020-06-08 17:48:47 +0800

    修复超宠task的done初始值

--- src/main/java/com/teeqee/net/handler/Session.java
+++ src/main/java/com/teeqee/net/handler/Session.java
@@ -21,7 +21,7 @@ import java.util.Map;
  * @Software: IntelliJ IDEA
  */
 
-public class Session<T>  {
+public class Session {
     /**因为超宠是登录后再传openId的*/
     private Long uid;
     /**平台默认本地服务器*/
@@ -44,6 +44,7 @@ public class Session<T>  {
         return false;
     }
 
+    /**发送数据源*/
     public void send(Object o){
         if (o!=null){
             String s = JSON.toJSONString(o);
@@ -132,28 +133,19 @@ public class Session<T>  {
         keyToAttrs.put(key, v);
     }
 
-    /**
-     * @return 返回用户的playerData
-     */
+    /**@return 返回用户的playerData*/
     public PlayerData getPlayerData(){
         return (PlayerData) keyToAttrs.get(PlayerCmd.PLAYER_DATA);
     }
-
-    /**
-     * @return 返回用户的playerInfo
-     */
+    /**@return 返回用户的playerInfo*/
     public PlayerInfo getPlayerInfo(){
         return (PlayerInfo) keyToAttrs.get(PlayerCmd.PLAYER_INFO);
     }
-    /**
-     * @return 返回用户的playerLog
-     */
+    /**@return 返回用户的playerLog*/
     public PlayerLog getPlayerLog(){
         return (PlayerLog) keyToAttrs.get(PlayerCmd.PLAYER_LOG);
     }
-    /**
-     * @return 返回用户的playerLog
-     */
+    /**@return 返回用户的playerLog*/
     public PlayerRank getPlayerRank(){
         return (PlayerRank) keyToAttrs.get(PlayerCmd.PLAYER_RANK);
     }
